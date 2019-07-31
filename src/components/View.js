ffd610a935b0e7a434125a722ccd0f2502f35976
mateo-navarrete <mateonavarrete@pursuit.org>
2019-07-31 12:28:02 -0400

    Add View class boilerplate

---
+++ src/components/View.js
@@ -0,0 +1,14 @@
+import { ufo } from '../db/ufo';
+
+export default class View {
+  constructor() {
+    this.messages = {};
+    this.rules = '';
+    this.title = '';
+    this.ufo = ufo;
+  }
+
+  render() {
+    //
+  }
+}
